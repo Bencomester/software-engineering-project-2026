f5e635f9d00c6ab84e46c8536ab14c44b8bba5e0
Bence László Szilágyi <bencomester@gmail.com>
2026-05-16 11:58:39 +0200

    Created BoardGameApplication.java to initialize the view

--- src/main/java/boardgame/Main.java
+++ src/main/java/boardgame/Main.java
@@ -1,16 +1,17 @@
 package boardgame;
 
 import boardgame.console.ConsoleGame;
+import javafx.application.Application;
 
 public class Main {
 
-    private static final boolean PLAY_IN_CONSOLE = true;
+    private static final boolean PLAY_IN_CONSOLE = false;
 
     public static void main(String[] args) {
         if (PLAY_IN_CONSOLE) {
             ConsoleGame.startConsoleGame();
         } else {
-            throw new RuntimeException("Currently only available in the console!");
+            Application.launch(BoardGameApplication.class, args);
         }
     }
 }
