0a62d9da6c0c0c219a6fd4a5b69fc81c0d297d04
Bence László Szilágyi <bencomester@gmail.com>
2026-05-16 12:52:06 +0200

    Added unit test for board reset and save/load

--- src/test/java/boardgame/model/UnitTest.java
+++ src/test/java/boardgame/model/UnitTest.java
@@ -4,138 +4,174 @@ import common.util.board.Position;
 import game.State.Status;
 import game.State.Player;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitTest {
 
     @Test
     void initialization() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
 
-        assertFalse(state.isGameOver());
-        assertEquals(Status.IN_PROGRESS, state.getStatus());
-        assertEquals("---\n---\n---\n", state.toString());
-        assertEquals(Player.PLAYER_1, state.getNextPlayer());
-        assertEquals(9, state.getLegalMoves().size());
+        assertFalse(model.isGameOver());
+        assertEquals(Status.IN_PROGRESS, model.getStatus());
+        assertEquals("---\n---\n---\n", model.toString());
+        assertEquals(Player.PLAYER_1, model.getNextPlayer());
+        assertEquals(9, model.getLegalMoves().size());
     }
 
     @Test
     void isLegalMove() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
 
-        assertTrue(state.isLegalMove(new Position(0, 0)));
-        assertTrue(state.isLegalMove(new Position(1, 1)));
-        assertTrue(state.isLegalMove(new Position(2, 2)));
+        assertTrue(model.isLegalMove(new Position(0, 0)));
+        assertTrue(model.isLegalMove(new Position(1, 1)));
+        assertTrue(model.isLegalMove(new Position(2, 2)));
 
-        assertFalse(state.isLegalMove(new Position(-1, 1)));
-        assertFalse(state.isLegalMove(new Position(1, 3)));
-        assertFalse(state.isLegalMove(new Position(3, 2)));
+        assertFalse(model.isLegalMove(new Position(-1, 1)));
+        assertFalse(model.isLegalMove(new Position(1, 3)));
+        assertFalse(model.isLegalMove(new Position(3, 2)));
     }
 
     @Test
     void legalMoves() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position nonePos = new Position(0, 0);
         Position redPos = new Position(1, 2);
         Position yellowPos = new Position(2, 1);
         Position greenPos = new Position(1, 1);
 
         assertDoesNotThrow(() -> {
-            state.makeMove(redPos);
-            state.makeMove(yellowPos);
-            state.makeMove(yellowPos);
-            state.makeMove(greenPos);
-            state.makeMove(greenPos);
-            state.makeMove(greenPos);
+            model.makeMove(redPos);
+            model.makeMove(yellowPos);
+            model.makeMove(yellowPos);
+            model.makeMove(greenPos);
+            model.makeMove(greenPos);
+            model.makeMove(greenPos);
         });
 
-        assertTrue(state.isLegalMove(nonePos));
-        assertTrue(state.isLegalMove(redPos));
-        assertTrue(state.isLegalMove(yellowPos));
-        assertFalse(state.isLegalMove(greenPos));
+        assertTrue(model.isLegalMove(nonePos));
+        assertTrue(model.isLegalMove(redPos));
+        assertTrue(model.isLegalMove(yellowPos));
+        assertFalse(model.isLegalMove(greenPos));
     }
 
     @Test
     void makeMove() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position pos = new Position(0, 0);
 
-        state.makeMove(pos);
-        assertEquals(Player.PLAYER_2, state.getNextPlayer());
-        assertEquals("R--\n---\n---\n", state.toString());
+        model.makeMove(pos);
+        assertEquals(Player.PLAYER_2, model.getNextPlayer());
+        assertEquals("R--\n---\n---\n", model.toString());
 
-        state.makeMove(pos);
-        assertEquals(Player.PLAYER_1, state.getNextPlayer());
-        assertEquals("Y--\n---\n---\n", state.toString());
+        model.makeMove(pos);
+        assertEquals(Player.PLAYER_1, model.getNextPlayer());
+        assertEquals("Y--\n---\n---\n", model.toString());
 
-        state.makeMove(pos);
-        assertEquals(Player.PLAYER_2, state.getNextPlayer());
-        assertEquals("G--\n---\n---\n", state.toString());
+        model.makeMove(pos);
+        assertEquals(Player.PLAYER_2, model.getNextPlayer());
+        assertEquals("G--\n---\n---\n", model.toString());
 
-        assertThrows(IllegalArgumentException.class, () -> state.makeMove(pos));
+        assertThrows(IllegalArgumentException.class, () -> model.makeMove(pos));
     }
 
     @Test
     void gameOverWithRow() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position pos1 = new Position(0, 0);
         Position pos2 = new Position(0, 1);
         Position pos3 = new Position(0, 2);
 
-        state.makeMove(pos1);
-        state.makeMove(pos2);
-        state.makeMove(pos3);
+        model.makeMove(pos1);
+        model.makeMove(pos2);
+        model.makeMove(pos3);
 
-        assertEquals(Status.PLAYER_1_WINS, state.getStatus());
-        assertTrue(state.getLegalMoves().isEmpty());
-        assertThrows(IllegalArgumentException.class, () -> state.makeMove(pos1));
+        assertEquals(Status.PLAYER_1_WINS, model.getStatus());
+        assertTrue(model.getLegalMoves().isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> model.makeMove(pos1));
     }
 
     @Test
     void gameOverWithColumn() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position pos1 = new Position(0, 0);
         Position pos2 = new Position(1, 0);
         Position pos3 = new Position(2, 0);
 
-        state.makeMove(pos1);
-        state.makeMove(pos2);
-        state.makeMove(pos3);
+        model.makeMove(pos1);
+        model.makeMove(pos2);
+        model.makeMove(pos3);
 
-        assertEquals(Status.PLAYER_1_WINS, state.getStatus());
-        assertTrue(state.getLegalMoves().isEmpty());
-        assertThrows(IllegalArgumentException.class, () -> state.makeMove(pos1));
+        assertEquals(Status.PLAYER_1_WINS, model.getStatus());
+        assertTrue(model.getLegalMoves().isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> model.makeMove(pos1));
     }
 
     @Test
     void gameOverWithDiagonal1() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position pos1 = new Position(0, 0);
         Position pos2 = new Position(1, 1);
         Position pos3 = new Position(2, 2);
 
-        state.makeMove(pos1);
-        state.makeMove(pos2);
-        state.makeMove(pos3);
+        model.makeMove(pos1);
+        model.makeMove(pos2);
+        model.makeMove(pos3);
 
-        assertEquals(Status.PLAYER_1_WINS, state.getStatus());
-        assertTrue(state.getLegalMoves().isEmpty());
-        assertThrows(IllegalArgumentException.class, () -> state.makeMove(pos1));
+        assertEquals(Status.PLAYER_1_WINS, model.getStatus());
+        assertTrue(model.getLegalMoves().isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> model.makeMove(pos1));
     }
 
     @Test
     void gameOverWithDiagonal2() {
-        BoardGameModel state = new BoardGameModel();
+        BoardGameModel model = new BoardGameModel();
         Position pos1 = new Position(2, 0);
         Position pos2 = new Position(1, 1);
         Position pos3 = new Position(0, 2);
 
-        state.makeMove(pos1);
-        state.makeMove(pos2);
-        state.makeMove(pos3);
+        model.makeMove(pos1);
+        model.makeMove(pos2);
+        model.makeMove(pos3);
 
-        assertEquals(Status.PLAYER_1_WINS, state.getStatus());
-        assertTrue(state.getLegalMoves().isEmpty());
-        assertThrows(IllegalArgumentException.class, () -> state.makeMove(pos1));
+        assertEquals(Status.PLAYER_1_WINS, model.getStatus());
+        assertTrue(model.getLegalMoves().isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> model.makeMove(pos1));
+    }
+
+    @Test
+    void resetGameBoard() {
+        BoardGameModel model = new BoardGameModel();
+        Position pos1 = new Position(1, 1);
+
+        model.makeMove(pos1);
+        model.resetGameBoard();
+
+        assertEquals(Player.PLAYER_1, model.getNextPlayer());
+        assertEquals("---\n---\n---\n", model.toString());
+    }
+
+    @Test
+    void saveAndLoadGaneState(@TempDir Path tempDir) throws IOException {
+        File saveFile = tempDir.resolve("temp_save.json").toFile();
+        BoardGameModel model = new BoardGameModel();
+        Position pos1 = new Position(0, 0);
+
+        model.makeMove(pos1);
+        model.saveGameStateToFile(saveFile);
+        model = new BoardGameModel();
+        model.loadGameStateFromFile(saveFile);
+
+        assertTrue(saveFile.exists());
+        assertTrue(saveFile.isFile());
+        assertTrue(saveFile.length() > 0);
+        assertEquals(Player.PLAYER_2, model.getNextPlayer());
+        assertEquals("R--\n---\n---\n", model.toString());
     }
 }
