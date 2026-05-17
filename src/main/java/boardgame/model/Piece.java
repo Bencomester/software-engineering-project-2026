6a0f934b289280eb4c8b4f1174729ee4c9e56bba
Bence László Szilágyi <bencomester@gmail.com>
2026-05-17 19:37:04 +0200

    Added javadoc to Piece.java to make it compliant with checkstyle

--- src/main/java/boardgame/model/Piece.java
+++ src/main/java/boardgame/model/Piece.java
@@ -1,8 +1,27 @@
 package boardgame.model;
 
+/**
+ * Represents the different piece types possible on the board.
+ */
 public enum Piece {
+
+    /**
+     * Represents an empty space, without a piece.
+     */
     NONE,
+
+    /**
+     * Represents a red piece.
+     */
     RED,
+
+    /**
+     * Represents a yellow piece.
+     */
     YELLOW,
+
+    /**
+     * Represents a green piece.
+     */
     GREEN
 }
