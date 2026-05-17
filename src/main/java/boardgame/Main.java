a19882d7f1cae2536e57d2f66e8e9a7c2a04ccd5
Bence László Szilágyi <bencomester@gmail.com>
2026-05-17 13:59:04 +0200

    Added logging to Main.java

--- src/main/java/boardgame/Main.java
+++ src/main/java/boardgame/Main.java
@@ -2,6 +2,7 @@ package boardgame;
 
 import boardgame.console.ConsoleGame;
 import javafx.application.Application;
+import org.tinylog.Logger;
 
 public class Main {
 
@@ -9,8 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
         if (PLAY_IN_CONSOLE) {
+            Logger.info("A console game has been started");
             ConsoleGame.startConsoleGame();
         } else {
+            Logger.info("A GUI game has been started");
             Application.launch(BoardGameApplication.class, args);
         }
     }
