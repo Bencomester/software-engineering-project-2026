df034c3886bfd9ff357005774a4a38adde48d2ac
Bence László Szilágyi <bencomester@gmail.com>
2026-05-17 19:46:11 +0200

    Added package info to boardgame

---
+++ src/main/java/boardgame/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains everything needed to play the board game.
+ * The board game application is made in an MVC like design,
+ * the model can be used playing from the console or in a GUI.
+ */
+package boardgame;
