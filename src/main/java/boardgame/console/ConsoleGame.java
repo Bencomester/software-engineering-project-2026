27fc9c463c34f6f048ad65f81f75aa82c9dbe965
Bence László Szilágyi <bencomester@gmail.com>
2026-05-17 19:12:01 +0200

    Added documentation to ConsoleGame.java while considering checkstyle requirements

--- src/main/java/boardgame/console/ConsoleGame.java
+++ src/main/java/boardgame/console/ConsoleGame.java
@@ -7,15 +7,36 @@ import game.console.Game;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ConsoleGame {
+/**
+ * Makes possible to play the board game from the console.
+ */
+public final class ConsoleGame {
 
+    /**
+     * Replaces default constructor, which is desirable for a utility class.
+     */
+    private ConsoleGame() { }
+
+    /**
+     * Starts the board game played in the console.
+     */
     public static void startConsoleGame() {
         BoardGameModel model = new BoardGameModel();
-        Game<Position, BoardGameModel> game = new Game<>(model, ConsoleGame::parseMoveFromString);
+        Game<Position, BoardGameModel> game =
+                new Game<>(model, ConsoleGame::parseMoveFromString);
         game.start();
     }
 
-    public static Position parseMoveFromString(String moveString) {
+    /**
+     * Converts string inputs to {@link Position}.
+     * The string should be in the following format: {@code n m},
+     * where n and m are integers.
+     * @param moveString input string to convert
+     * @return a position matching the input string
+     * @throws IllegalArgumentException if the string doesn't match
+     * the required format
+     */
+    private static Position parseMoveFromString(final String moveString) {
         Scanner sc = new Scanner(moveString);
         try {
             int x = sc.nextInt();
